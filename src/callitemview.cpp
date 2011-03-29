d7e1749c0afb18ef7572c89a610db45843407474
Shane Bryan <shane.bryan@linux.intel.com>
2011-03-29 16:59:25 -0700

    Fixed:   BMC# 14948 - The system time is displayed before the call is actived
    Signed-off-by: Shane Bryan <shane.bryan@linux.intel.com>

--- src/callitemview.cpp
+++ src/callitemview.cpp
@@ -121,14 +121,11 @@ void CallItemView::initLayout()
     layout()->addItem(spacer,          1, 0, 1, 1, Qt::AlignCenter);
     layout()->addItem(statusLabel(),   1, 1, 1, 1, Qt::AlignRight);
     layout()->addItem(durationLabel(), 1, 2, 1, 1, Qt::AlignRight);
-
-    m_updateTimer.start(1000);
-    connect(&m_updateTimer, SIGNAL(timeout()), SLOT(updateDurationLabel()));
 }
 
 void CallItemView::updateDurationLabel()
 {
-    QTime t = QTime(0,0).addSecs(m_controller->duration()/1000);
+    QTime t = QTime(0,0).addSecs(m_controller->duration());
     durationLabel()->setText(t.toString(Qt::TextDate));
 }
 
@@ -176,6 +173,17 @@ void CallItemView::updateStatusLabel()
         qDebug("CallItemView: setModeDefault()");
         style().setModeDefault();
         break;
+    }
+
+    // We only start tracking the duration for connected (Active or Held) calls
+    bool trackDuration = ((model()->stateType()==CallItemModel::STATE_ACTIVE)||
+                          (model()->stateType()==CallItemModel::STATE_HELD));
+    if (trackDuration && !m_updateTimer.isActive()) {
+        m_updateTimer.start(1000);
+        connect(&m_updateTimer, SIGNAL(timeout()), SLOT(updateDurationLabel()));
+    } else if (!trackDuration && m_updateTimer.isActive()) {
+        disconnect(&m_updateTimer, SIGNAL(timeout()));
+        m_updateTimer.stop();
     }
 
     applyStyle();
--- src/callproxy.cpp
+++ src/callproxy.cpp
@@ -98,7 +98,10 @@ QDateTime CallProxy::startTime() const
 int CallProxy::duration() const
 {
     TRACE
-    return m_startTime.time().elapsed();
+    if (m_startTime.isValid())
+        return m_startTime.secsTo(QDateTime::currentDateTime());
+    else
+        return 0;
 }
 
 QString CallProxy::reason() const
@@ -268,29 +271,10 @@ void CallProxy::disconnectReason(const QString &in0)
 void CallProxy::setStartTimeFromString(const QString &val)
 {
     TRACE
-    if (val.isEmpty())
-        return;
-
-    // NOTE: QDateTime::fromString(val, Qt::ISODate) Fails since the date
-    //       format from Ofono is in RFC 822 form, but QDateTime can't parse it
-    // NOTE: Ofono formats time to string with the following format spec:
-    //       %Y-%m-%dT%H:%M:%S%z
-    struct tm time_tm;
-    QByteArray  bytes = val.toAscii();
-    const char *str = bytes.constData();
-    if (strptime(str, "%Y-%m-%dT%H:%M:%S%z", &time_tm) != NULL) {
-        time_t t = mktime(&time_tm);
-        if (t >= (time_t)(0))
-            m_startTime.setTime_t(t);
-    }
+    m_startTime = qDateTimeFromOfono(val);
 
     if (!m_startTime.isValid())
-        qCritical() << QString("Format error, could not parse: %1").arg(str);
-    else
-        // FIXME: This could be a pre-existing call (i.e. a call is in progress
-        //        when we are started), in which case this start is not really
-        //        in sync with the *actuall* start time.
-        m_startTime.time().start(); // begin elapsed time timer
+        m_startTime = QDateTime::QDateTime::currentDateTime();
 }
 
 
